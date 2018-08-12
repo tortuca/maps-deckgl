47039c7505f4ae537fceb3bba87b055a6d525caf
Amanda Chow <amandajt_chow@certissecurity.com>
2018-08-12 22:03:37 +0800

    Render data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
-import App from './map';
+import App from './App';
+import HexMap from './hex-map';
+import {renderToDOM} from "./hex-map";
 // import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<HexMap />, document.getElementById('app'));
+renderToDOM(document.getElementById('app'));
 // registerServiceWorker();
